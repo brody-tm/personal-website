aefc9ad85330bc3bc730bff95a34077d62b9e002
brodytrue <btm00005@mix.wvu.edu>
2024-11-18 23:39:48 -0500

    Made dark mode persistent, some layout changes

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-function toggleDarkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    localStorage.mode = "dark;"
+if (!localStorage.getItem("mode")) {
+  console.log("No mode");
+  localStorage.setItem("mode", "dark")
+}
 
-    document.querySelectorAll(".dark-mode-button").forEach(element => {
-        element.classList.add("dark-mode");
-      });
- }
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("DOM content loaded");
+
+  const element = document.body;
+
+  if (localStorage.getItem("mode") == "dark") {
+    element.classList.add("dark-mode");
+    console.log("Dark mode automatically enabled");
+  }
+
+  if (localStorage.getItem("mode") == "light") {
+    element.classList.add("light-mode");
+    console.log("Light mode automatically enabled");
+  }
+});
+
+function toggleDarkMode() {
+    const element = document.body;
+
+    if (localStorage.getItem("mode") == "dark") {
+      element.classList.remove("dark-mode");
+      element.classList.add("light-mode");
+      localStorage.setItem("mode", "light");
+      console.log("Dark mode -> light mode");
+    } else {
+      element.classList.remove("light-mode");
+      element.classList.add("dark-mode");
+      localStorage.setItem("mode", "dark");
+      console.log("Light mode -> dark mode");
+    }
+}
